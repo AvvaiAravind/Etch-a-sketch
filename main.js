b3b4f690c2ef8445b0dded85b588a3592c59de37
Avvai Aravind <avvaiaravind@gmail.com>
2024-09-24 12:34:19 +0530

    added mouse over hover effect

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-const board = document.querySelector(".board");
-const gridRow = 20;
-const gridColumn = 50;
-const gridSize = gridColumn * gridRow;
+const select = document.querySelector("#popup");
+select.addEventListener("click", getGridSize);
 
-for (let i = 0; i < gridSize; i++) {
-  let div = document.createElement("div");
+function getGridSize() {
+  const gridSize = prompt("Enter a GridSize maximum upto 100");
+  const message = document.querySelector(".message");
+  if (gridSize === "" || gridSize === null || gridSize > 100 || gridSize < 1) {
+    message.textContent = "Please Select a valid input between 1 to 100";
+  } else {
+    message.textContent = "Now you can play!";
+    createGrid(gridSize);
+  }
+}
 
-  div.style.flexBasis = `${100 / gridColumn}%`;
-  div.style.height = `${100 / gridRow}%`;
-  div.style.backgroundColor = "yellow";
-  board.appendChild(div);
+function createGrid(gridSize) {
+  const board = document.querySelector(".board");
+  /* const boardSize = Math.max(gridSize * 20, 500);
+  board.style.maxWidth = `${boardSize}px`;
+  board.style.maxHeight = `${boardSize}px`; */
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let div = document.createElement("div");
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "black";
+    });
+    div.style.flexBasis = `${100 / gridSize}%`;
+    div.style.height = `${100 / gridSize}%`;
+    div.style.backgroundColor = "yellow";
+    board.appendChild(div);
+  }
 }
